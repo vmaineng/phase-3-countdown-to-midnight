70b75813253730aef84fe9b4561d082fa9ce4841
Mai Vang <vmaineng@gmail.com>
2022-06-12 15:54:04 -0700

    commit tree 5f534a664f543de468f5bcf742fb162351d804f6

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+def countdown(value)
+    while value > 0
+         puts "#{value} SECOND(S)!"
+        value -= 1
+    end
+     'HAPPY NEW YEAR!'
+
+end
+
+def countdown_with_sleep(value)
+    while value > 0
+        puts "#{value} SECOND(S)!"
+        sleep(3)
+        value -= 1
+    end
+    'HAPPY NEW YEAR!'
 end
